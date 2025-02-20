be76838af472778efb475388c01fb2d0eea5b731
Aditya Rai <aditya0910rai@gmail.com>
2025-02-20 23:08:13 +0530

    first commit

---
+++ DEMO.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class DEMO {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt ();
+        System.out.println("Hey " + x);
+        scanner.close();
+    }
+}
