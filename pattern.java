80bb91b8a7baaf3c8371807c06aeef1bbae19f52
Aditya Rai <75214652+rai0910@users.noreply.github.com>
2025-03-14 01:20:42 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,12 @@
+package course;
+
+public class pattern {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+    }
+    
+}
