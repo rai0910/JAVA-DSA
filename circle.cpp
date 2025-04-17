d5d1241c6eb4e7a9ec014387e837e98491593cf3
Aditya Rai <75214652+rai0910@users.noreply.github.com>
2025-04-17 22:46:58 +0530

    Add files via upload

---
+++ circle.cpp
@@ -0,0 +1,10 @@
+#include<graphics.h>
+int main(int argc, char const *argv[])
+{
+    int gd=DETECT,gm;
+    initgraph(&gd,&gm,(char*)"");
+    circle(300,240,200);
+    getch();
+    closegraph();
+    return 0;
+}
